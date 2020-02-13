3dc1e1593d3b9c0a8da6a3a4fe4806a27f829f7a
genefis <genesisgonzalez492@gmail.com>
2020-02-13 10:07:22 -0500

    tarea

--- poo2020/src/Deber_seleccion/Ejecuta_sueldotriple.java
+++ poo2020/src/Deber_seleccion/Ejecuta_sueldotriple.java
@@ -1,18 +1,16 @@
-package disenio_seleccion;
-
+package Deber_seleccion;
 import java.util.Scanner;
 
-public class Ejecuta_sueldo {
+public class Ejecuta_sueldotriple {
     public static void main(String[] args) {
         Scanner ent = new Scanner (System.in);
-
         System.out.println("INGRESE EL NOMBRE");
         String nombre = ent.nextLine();
         System.out.println("INGRESE EL NUMERO DE HORAS");
         int  horas =ent.nextInt();
         System.out.println("INGRESE LA CUOTA X HORA");
         Double cuota =ent.nextDouble();
-        Sueldo sueldo = new Sueldo(cuota,horas,nombre);
-        System.out.println("El sueldo correspondiente es "+sueldo.calcular_sueldo());
+        Sueldo_triple  sueldotriple = new Sueldo_triple(cuota,horas,nombre);
+        System.out.println("El sueldo correspondiente es "+sueldotriple.calcular_sueldo());
     }
 }
