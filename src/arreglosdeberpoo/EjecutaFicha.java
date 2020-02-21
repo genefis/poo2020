7296978456d8dac19c3b7eb2b3db9b90fab30254
genefis <genesisgonzalez492@gmail.com>
2020-02-21 08:31:55 -0500

    arreglospootarea

---
+++ src/arreglosdeberpoo/EjecutaFicha.java
@@ -0,0 +1,44 @@
+package arreglosdeberpoo;
+
+import java.util.Scanner;
+
+public class EjecutaFicha {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        /**
+         * Declaracion de variables
+         */
+        String nombre = "";
+        String edad = "";
+        String universidad = "";
+        String celular = "";
+        int contador;
+        /**
+         * creacion de objeto Ficha
+         */
+        ficha ficha = new ficha();
+        /**
+         * Obtencion de datos mediante un ciclo repetitivo
+         */
+        for (contador = 0; contador <= 4; contador++) {
+            System.out.println("Ingrese al estudiante " + contador);
+            System.out.println("Ingrese el nombre");
+            nombre = sc.nextLine();
+            System.out.println("Ingrese la edad");
+            edad = sc.nextLine();
+            System.out.println("Ingrese las iniciales de la universidad");
+            universidad = sc.nextLine();
+            System.out.println("ingrese la cedula");
+            celular = sc.nextLine();
+            /**
+             * Envio de datos al objeto
+             */
+            ficha.obtenerDatos(nombre, edad, universidad, celular);
+            ficha.almacenarArreglo(contador);
+        }
+        /**
+         * Presentacion de el objeto
+         */
+        ficha.presentar();
+    }
+}
